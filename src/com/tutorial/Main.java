3c132524a026145a386b841735f8e366bbe85022
ferdi <ferdid047@gmail.com>
2022-11-13 09:38:20 -0800

    Initial commit

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,12 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Deklarasi array
+        String[] mahasiswa = {"Rini","Aldi","Acep","ferdi"};
+
+        //Menampilkan isi array pada indeks ke-2
+        System.out.println(mahasiswa[2]);
+    }
+}
